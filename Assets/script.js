a732e3f999301e3ba25aa25f0cc13cad28d048cb
Chase Johnson <Chase.b.johnson@gmail.com>
2020-05-01 14:19:31 -0700

    Local Storage Functionality Added

--- Assets/script.js
+++ Assets/script.js
@@ -12,4 +12,15 @@ $(document).ready(function(){
 
         //set items in local storage.
         localStorage.setItem(time, text);
-    
+    })
+
+    // load saved data from LocalStorage for each hour //
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
