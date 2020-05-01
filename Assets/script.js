dc3da6cecb8f085f1ec51c7981abff778b36aca8
Chase Johnson <Chase.b.johnson@gmail.com>
2020-05-01 15:01:24 -0700

    Game is working! Almost finished

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,25 @@
+
 $(document).ready(function(){
 
-    //display current day & time.
+    // Display current day & time //
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    //Assign saveBtn click listener for user input and time stamp??
+    // Assign saveBtn click listener for user input and time stamp //
     $(".saveBtn").on("click", function () {
-        //get nearby values.
+        // Get nearby values
         console.log(this);
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        //set items in local storage.
+        // Set items in local storage //
         localStorage.setItem(time, text);
     })
+
+    //button to clear local storage and refresh the page
+    $("#clear").click(function() {
+        localStorage.clear();
+        location.reload()
+    });
 
     // load saved data from LocalStorage for each hour //
     $("#hour9 .description").val(localStorage.getItem("hour9"));
@@ -25,16 +32,16 @@ $(document).ready(function(){
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-    //get current number of hours.
+    // get current number of hours //
     function hourTracker() {
         var currentHour = moment().hour();
 
-     // loop over time blocks
+     // loop over time blocks //
      $(".time-block").each(function () {
         var blockHour = parseInt($(this).attr("id").split("hour")[1]);
         console.log( blockHour, currentHour)
 
-    //check if we've moved past this time
+    // check if we've moved past this time //
       if (blockHour < currentHour) {
         $(this).addClass("past");
         $(this).removeClass("future");
