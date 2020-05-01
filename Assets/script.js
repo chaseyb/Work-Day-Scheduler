b0d703b99d89fc3882886eace8e70286cf52d157
Chase Johnson <Chase.b.johnson@gmail.com>
2020-05-01 14:28:19 -0700

    Updated HTML and JavaScript

--- Assets/script.js
+++ Assets/script.js
@@ -33,3 +33,25 @@ $(document).ready(function(){
      $(".time-block").each(function () {
         var blockHour = parseInt($(this).attr("id").split("hour")[1]);
         console.log( blockHour, currentHour)
+
+    //check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+    }
+    else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }
+})
+}
+hourTracker();
+})
+
